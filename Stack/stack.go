9cd10699aa642f8c48089e7fb313d592787130f0
Amin-mhr <amin.mhr2001@gmail.com>
2026-02-18 23:02:48 +0330

    stack exercisee

--- Stack/stack.go
+++ Stack/stack.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import "fmt"
+
+type Stack struct {
+	data []interface{}
+}
+
+func (s *Stack) Pop() interface{} {
+	if len(s.data) == 0 {
+		return nil
+	} else {
+		v := s.data[len(s.data)-1]
+		s.data = s.data[:len(s.data)-1]
+		return v
+	}
+}
+
+func (s *Stack) Push(v interface{}) {
+	s.data = append(s.data, v)
+}
+
+func (s *Stack) Length() int {
+	return len(s.data)
+}
+
+func main() {
+	stack := Stack{}
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	fmt.Println(stack.Length())
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Length())
+	fmt.Println(stack.Pop())
+}
